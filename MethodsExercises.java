17a865c8de554291c551a6bacf07ff2fefcc66ae
Emilia Mata <mataemilia2609@gmail.com>
2023-06-16 08:45:04 -0600

    MethodExercises

---
+++ MethodsExercises.java
@@ -0,0 +1,12 @@
+public class MethodsExercises {
+    public static void sayHello(String[] args) {
+        return String.format("Hello, %s!", name):
+
+    public static void sayHello(String[] args) {
+            System.out.println(sayHello("Emily"));
+            System.out.println(sayHello());
+            sayHello(3);
+
+        }
+    }
+}
