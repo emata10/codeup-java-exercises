d608ddbaaa76b12cc6b4ee53de74ebdee00b058e
Emilia Mata <mataemilia2609@gmail.com>
2023-06-20 08:49:09 -0600

    GameDevelopment

--- src/HighLowExercise.java
+++ src/HighLowExercise.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class MethodsExercises {
+class HighLow {
 //    public static void main(String[] args) {
 ///////Part 1//////
 
@@ -49,7 +49,7 @@ public class MethodsExercises {
 //
 
     ////////Part Three////
-//    public static int multiply(int a int b) {
+//    public static int multiply(int an int b) {
 //        int result = 0;
 //        boolean isNegative = false;
 //
@@ -87,53 +87,55 @@ public class MethodsExercises {
 
 
 
-        // Method for multiplication using loop
-        public static int multiplyLoop(int x, int y) {
-            int product = 0;
-            for (int i = 0; i < y; i++) {
-                product += x;
-            }
-            return product;
+    // Method for multiplication using loop
+    public static int multiplyLoop(int x, int y) {
+        int product = 0;
+        for (int i = 0; i < y; i++) {
+            product += x;
         }
+        return product;
+    }
 
-        // Method for multiplication using recursion
-        public static int multiplyRecursive(int x, int y) {
-            if (x == 0 || y == 0) {
-                return 0;
-            } else if (y > 0) {
-                return x + multiplyRecursive(x, y - 1);
-            } else {
-                return -multiplyRecursive(x, -y);
-            }
-        }
-
-        // Method for validating user input in a certain range
-        public static int getInteger(int min, int max) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a number between " + min + " and " + max + ": ");
-            int input = scanner.nextInt();
-            if (input < min || input > max) {
-                System.out.println("Invalid input!");
-                input = getInteger(min, max); // Recursion
-            }
-            return input;
-        }
-
-        // Main method
-        public static void main(String[] args) {
-            int x = 5;
-            int y = 4;
-
-            // Multiplication using loop
-            System.out.println("Using loop: " + multiplyLoop(x, y));
-
-            // Multiplication using recursion
-            System.out.println("Using recursion: " + multiplyRecursive(x, y));
-
-            // Validating user input
-            int userInput = getInteger(1, 10);
-            System.out.println("User input: " + userInput);
+    // Method for multiplication using recursion
+    public static int multiplyRecursive(int x, int y) {
+        if (x == 0 || y == 0) {
+            return 0;
+        } else if (y > 0) {
+            return x + multiplyRecursive(x, y - 1);
+        } else {
+            return -multiplyRecursive(x, -y);
         }
     }
+
+    // Method for validating user input in a certain range
+    public static int getInteger(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between " + min + " and " + max + ": ");
+        int input = scanner.nextInt();
+        if (input < min || input > max) {
+            System.out.println("Invalid input!");
+            input = getInteger(min, max); // Recursion
+        }
+        return input;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4;
+
+        // Multiplication using loop
+        System.out.println("Using loop: " + multiplyLoop(x, y));
+
+        // Multiplication using recursion
+        System.out.println("Using recursion: " + multiplyRecursive(x, y));
+
+        // Validating user input
+        int userInput = getInteger(1, 10);
+        System.out.println("User input: " + userInput);
+    }
+}
+
+
 
 
