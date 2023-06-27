ec57773d2f980cbc5133d065af1dc04dbb541fd1
Emilia Mata <mataemilia2609@gmail.com>
2023-06-27 14:06:39 -0600

    Square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,24 +1,8 @@
 package shapes;
 ////The package shapes; statement indicates that this class belongs to the "shapes" package.//
-
-public class Square extends Rectangle {
-
-    public Square(double side) {
-        super(side, side);
-    }
-    public double getArea(){
-        System.out.println("Square Method Used");
-        double side = this.width;
-        return side * side;
-    }
-    public double getPerimeter(){
-        System.out.println("Square Method used");
-        double side = this.width;
-        return side * 4;
-
-    }
-
-
+interface Measurable {
+    double getPerimeter();
+    double getArea();
 }
 
 
